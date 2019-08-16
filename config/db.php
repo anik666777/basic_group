c3e07dd8356028c4474a9daaa14e963e43c777e8
Anik <anik777666@gmail.com>
2019-08-16 12:45:57 +0300

    complete

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=basic_group',
+    'username' => 'basic_user',
+    'password' => 'Uh&@hs.Q>;n;Z3<z',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+
+
